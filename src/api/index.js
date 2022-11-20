b27f0a5f033a5b560373705f8bd4529b0f23738a
Szymon Zieba <ssajzmonq@gmail.com>
2022-11-20 19:47:23 +0100

    https

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 export const fetchAllProducts = async () => {
     try{
-        const data = await fetch("http://www.mocky.io/v2/5ab0d1882e0000e60ae8b7a6")
+        const data = await fetch("https://www.mocky.io/v2/5ab0d1882e0000e60ae8b7a6")
         return await data.json()
     }catch(err){
        console.error(err)
@@ -9,7 +9,7 @@ export const fetchAllProducts = async () => {
 
 export const fetchProduct = async (id) => {
     try{
-        const data = await fetch("http://www.mocky.io/v2/5ab0d1882e0000e60ae8b7a6")
+        const data = await fetch("https://www.mocky.io/v2/5ab0d1882e0000e60ae8b7a6")
         const parsedData = await data.json()
         const filteredData = parsedData.filter(item => {
             return item.id == id
